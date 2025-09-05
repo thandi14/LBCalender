00cd8bd23680fc93c3fe012793092689b901d808
thandi mpofu <thandimpofu2003@gmail.com>
2025-09-05 16:06:06 -0500

    Initial commit

---
+++ syllabus-parser/src/app/Calender/page.tsx
@@ -0,0 +1,30 @@
+
+'use client';
+
+import { useState } from 'react';
+
+export default function Calendar() {
+  const [assignments, setAssignments] = useState<{ title: string; dueDate: string }[]>([]);
+
+  const fetchAssignments = async () => {
+    const res = await fetch('/api', { method: 'GET' });
+    const data = await res.json();
+    setAssignments(data);
+  };
+
+  console.log(assignments)
+
+  return (
+    <div>
+      <h1>My Calendar</h1>
+      <button onClick={fetchAssignments}>Load Assignments</button>
+      <ul>
+        {assignments.map((a, i) => (
+          <li key={i}>
+            {a.title} - {a.dueDate}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
